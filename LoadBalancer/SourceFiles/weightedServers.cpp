666ac827528b2c4215a5eaadb2f14b194dd27dfe
Calvin Langat <calvokkt@gmail.com>
2024-07-15 19:12:51 +0100

    Working weighted Round Robin

--- LoadBalancer/SourceFiles/weightedServers.cpp
+++ LoadBalancer/SourceFiles/weightedServers.cpp
@@ -4,7 +4,6 @@
 
 void createServer();
 
-std::vector<server*> serverList;
 
 class server{
 public:
@@ -16,7 +15,7 @@ public:
 
 public:
     // Constructor
-    server(int serverID, int performance){
+    server(int serverID, float performance){
         this->serverID = serverID;
         this->performance = performance;
 
@@ -30,22 +29,74 @@ public:
     }
 };
 
+std::vector<server*> serverList;
+
 int main(){
 
     // Spawn the servers
+    server server1 = server(1, 1);
+    server server2 = server(2, 0.1);
+    server server3 = server(3, 1);
+    server server4 = server(4, 1);
+    server server5 = server(5, 1);
+    server server6 = server(6, 1);
+    server server7 = server(7, 1);
+    server server8 = server(8, 1);
+
+    serverList.push_back(&server1);
+    serverList.push_back(&server2);
+    serverList.push_back(&server3);
+    serverList.push_back(&server4);
+    serverList.push_back(&server5);
+    serverList.push_back(&server6);
+    serverList.push_back(&server7);
+    serverList.push_back(&server8);
     
-    
-    
+    float ratio;
+    bool isAssigned = false; 
+
     // Create a while loop to run for fixed amount of time
     int i = 0;
-    while(i < 1E6){
+    int j = 0;
+    int serverRequests = 0;
+    int serverSize = 0;
+    while(i < 1E7){
+        serverSize = serverList.size();
+        if (j > serverSize-1){
+            j = 0;
+        }
+        isAssigned = false;
         
+        while (!isAssigned)
+        {
+            serverRequests = serverList[j]->serverRequests;
+            if (serverRequests == 0){
+                ratio = 0;
+            }
+            else{
+                ratio = (float)serverRequests / (float)i;
+            }
+            
+            if (ratio - serverList[j]->performance <= 0){
+                serverList[j]->serverRequests += 1;
+                isAssigned = true;
+                break;
+            }
+            else{
+                j++;
+                if (j>serverSize-1){
+                    j = 0;
+                }
 
+            }
+        }
+
+        j++;
         i++;
     }
 
     for (const auto& k:serverList){
-        //std::cout << "Server "<< k << " requests: "<< k << "\n";
+        std::cout << "Server "<< k->serverID << " requests: "<< k->serverRequests << "\n";
     }
 
     return 0;
